19a9d23a31bee5482fcb6f3f0a1c0dff7aaf2632
Anton <antonk1973@icloud.com>
2022-09-20 10:07:15 +0300

    Initial Commit

---
+++ TrackSimulationTask/TrackSimulationTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackSimulationTaskApp.swift
+//  TrackSimulationTask
+//
+//  Created by Anton2016 on 20.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrackSimulationTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
